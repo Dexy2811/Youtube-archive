552ea832436590453e8325caa5bac90c9b6a09d6
Dexy <helpdextron@gmail.com>
2021-07-19 19:54:14 +0200

    Create Download Script.ps1

---
+++ Download Script.ps1
@@ -0,0 +1,4 @@
+$host.ui.RawUI.WindowTitle = 'Archive Script' # Changes title of window
+youtube-dl.exe LINK --cookies "C:\Path\To\Cookies\cookies.personal.txt"
+Write-Host "Script Finnished. Press enter to close."
+Pause
